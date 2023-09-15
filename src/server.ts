b570d58048b7ba5233d99488fdacdf2b42ac3cd6
danieldagerom <danielprimo9@gmail.com>
2023-09-15 14:31:31 -0300

    implementa função de upload

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from "fastify";
 import { getAllPromtsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify()
 
 app.register(getAllPromtsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({
     port:3333,
