ece961a5311fe3d49b25b211483f4257d4156f44
danieldagerom <danielprimo9@gmail.com>
2023-09-15 15:40:52 -0300

    integra api open ai e transcrição

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { getAllPromtsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify()
 
 app.register(getAllPromtsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
     port:3333,
